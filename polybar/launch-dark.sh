607e886b0d60241f54557bf8ada6b7fb7f22074e
Bleyom <57446911+Bleyom@users.noreply.github.com>
2021-10-04 09:09:34 -0500

    Add files via upload

---
+++ polybar/launch-dark.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar -c ~/.config/polybar/modules -r bar &
+polybar -c ~/.config/polybar/config-dark -r bar &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+	  polybar --reload config-dark &
+  polybar --reload modules
+	fi
+
+echo "Polybar launched..."
