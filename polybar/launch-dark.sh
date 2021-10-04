8dbc1255fef287113fad343e4c9440ee577c5642
Bleyom <57446911+Bleyom@users.noreply.github.com>
2021-10-04 09:10:13 -0500

    Update launch-dark.sh

--- polybar/launch-dark.sh
+++ polybar/launch-dark.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -c ~/.config/polybar/modules -r bar &
+polybar -c ~/.config/polybar/workspaces -r bar &
 polybar -c ~/.config/polybar/config-dark -r bar &
 
 if type "xrandr"; then
